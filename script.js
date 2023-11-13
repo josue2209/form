6326b412ec5969fcd0fb9b9044c20c5e781c6358
josue2209 <cabrerajosue93409@gmail.com>
2023-11-13 17:02:44 -0400

    firt commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function enviar() {
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Formulario enviado correctamente",
+        showConfirmButton: false,
+        timer: 2000
+      });
+}
